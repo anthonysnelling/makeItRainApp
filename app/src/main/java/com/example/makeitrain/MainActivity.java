434046be123b651f738496a5bd5f5f0d53ed7272
Anthony Snelling <anthony.snelling@live.com>
2021-04-23 17:50:14 -0400

    added toast, and snackbar notifications

--- app/src/main/java/com/example/makeitrain/MainActivity.java
+++ app/src/main/java/com/example/makeitrain/MainActivity.java
@@ -8,10 +8,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-    private Button makeItRain;
-    private Button showInfo;
+//    private Button makeItRain;
+//    private Button showInfo;
     private TextView moneyValue;
     private int moneycounter = 0;
 
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        makeItRain = findViewById(R.id.buttonMakeItRain);
+//        makeItRain = findViewById(R.id.buttonMakeItRain);
         moneyValue = findViewById(R.id.moneyValue);
 
 //        makeItRain.setOnClickListener(v -> Log.d("MainActivity", "onClick: Make It Rain"));
@@ -36,5 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void showInfo(View view) {
         Log.d("MainActivity", "onClick: show Info");
+        Toast.makeText(MainActivity.this, R.string.app_info, Toast.LENGTH_SHORT)
+                .show();
+
+        Snackbar.make(moneyValue, R.string.app_info,Snackbar.LENGTH_LONG)
+                .setAction("more", v -> {
+                    Log.d("Snack", "Show Info:SnackBar More");
+                })
+                .show();
     }
 }
